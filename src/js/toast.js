7e76b8e8cc151401e820562a520542b855e0a20b
Jad Joubran <jad.joubran@etobb.com>
2013-06-10 16:25:47 +0300

    First release - added 1 example page containing 2 demos

---
+++ src/js/toast.js
@@ -0,0 +1,17 @@
+function android_toast( message ){
+	if ( ! message ){
+		return false;
+	}
+	$("#android_toast").remove();
+	$("body").append("<div id='android_toast' class='android_toast_fadein'>" + message + "</div>");
+	$("#android_toast").css({
+		'margin-left': -parseInt(( $(window).width() * 0.8 / 2))	//fix center positionning. Cannot use a fix width in order to support small devices
+	});
+	setTimeout( android_toast_remove, 3000 );
+}
+
+function android_toast_remove(){
+	$("#android_toast").removeClass('android_toast_fadein').addClass('android_toast_fadeout').on('webkitAnimationEnd', function(){
+		$("#android_toast").remove();
+	});
+}
